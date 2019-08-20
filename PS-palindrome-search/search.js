86586ba5fdad2e128b2a7f8db5315d44e2233a7d
mharnetiaux <mattskyh@gmail.com>
2019-08-20 14:22:09 -0700

    update to code. Sorted and Filtered out duplicates before finding max and secondMax.

--- PS-palindrome-search/search.js
+++ PS-palindrome-search/search.js
@@ -1,37 +1,40 @@
-const str1 = "My dad is a racecar athlete";
+const str1 = "Mydadisaracecar athlete";
 const str2 = "Mom and brother.";
 const str3 = "";
 
 function palindrome(str) {
-  const obj = {};
-  const arr = [];
-  let max;
-  let secondMax;
-  str = str.replace(/ /g,'').toLowerCase();
-  for(let i = 0; i < str.length; i++) {
-    let beginning = str[i];
-    let occurance;
-    for(let j = i + 1; j < str.length; j++) {
-      if(beginning === str[j]) {
-        occurance = j;
-        let string = str.slice(i, occurance + 1);
-        let stringReverse = string.split('').reverse().join('');
-        if(string === stringReverse) {
-          obj[string.length] = string;
-          arr.push(string.length);
+    const obj = {};
+    let arr = [];
+    let arrLength;
+    let max;
+    let secondMax;
+    str = str.replace(/ /g,'').toLowerCase();
+    for(let i = 0; i < str.length; i++) {
+        let beginning = str[i];
+        let occurrence;
+        for(let j = i + 1; j < str.length; j++) {
+            if(beginning === str[j]) {
+                occurrence = j;
+                const string = str.slice(i, occurrence + 1);
+                const stringReverse = string.split('').reverse().join('');
+                if(string === stringReverse) {
+                  obj[string.length] = string;
+                  arr.push(string.length);
+                }
+            }
         }
-      }
     }
-  }
-  if(arr.length === 0){
-    return "No Palindrome exists"
-  }else if(arr.length === 1) {
-    return "No Second Palindrome exists"
-  }
-  max = Math.max(...arr);
-  arr.splice(arr.indexOf(max), 1);
-  secondMax = Math.max(...arr);
-  return `Found Palindrome: ${obj[secondMax]};`
+    arrLength = arr.length;
+    arr = arr.sort().filter((length, i) => arr.indexOf(length) === i);
+    if(arrLength === 0){
+        return "No Palindrome exists"
+    }else if(arrLength === 1) {
+        return "No Second Palindrome exists"
+    }
+    max = Math.max(...arr);
+    arr.splice(arr.indexOf(max), 1);
+    secondMax = Math.max(...arr);
+    return `Found Palindrome: ${obj[secondMax]};`
 }
 
 console.log(palindrome(str1));
