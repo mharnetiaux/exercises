0ee221792e75fc6159817e5d6625ed3dd2f23d27
mharnetiaux <mattskyh@gmail.com>
2019-09-20 17:20:45 -0700

    update to file structure.

--- UI-user-feed/src/index.js
+++ UI-user-feed/src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './state/store/store';
-
 import ChatApp from './components/ChatApp';
 
-// const store = configureStore();
 const store = configureStore();
 
 render(
