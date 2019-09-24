8df4bc5575cc6c48259a75c8d184d60d6697d2fa
mharnetiaux <mattskyh@gmail.com>
2019-09-23 20:18:22 -0700

    update to organization of state, logging.

--- UI-user-feed/src/state/middleware/getMessages.js
+++ UI-user-feed/src/state/middleware/getMessages.js
@@ -4,11 +4,11 @@ import { apiFetchDataSuccess } from "../actions/messages";
 const getMessagesMiddleware = store => next => action => {
     next(action);
     if (action.type !== API_START_FETCH) return;
-    console.log(`Redux middleware: API_START_FETCH called. ${ '\u221A' }`);
+    console.log(`Redux middleware ${'\u2192'} API_START_FETCH called. ${ '\u221A' }`);
 
     fetch(action.url)
         .then((response) => {
-            console.log(`HTTP response: ${ response.status } ${ response.ok }. ${ '\u221A' }`);
+            console.log(`HTTP response ${'\u2192'} ${ response.status } ${ response.ok }. ${ '\u221A' }`);
             return response.json();
         })
         .then((messages) => {
@@ -16,7 +16,7 @@ const getMessagesMiddleware = store => next => action => {
             store.dispatch(apiFetchDataSuccess(messages));
         })
         .catch((response) => {
-            console.log(`HTTP ERROR: ${ response } ${ '\u221A' }`);
+            console.log(`HTTP ERROR ${'\u2192'} ${ response } ${ '\u221A' }`);
             throw Error(`${ response }`);
         });
 };
