6b300fc17654829f50af816fd4a65c9111f2df8c
mharnetiaux <mattskyh@gmail.com>
2019-09-26 16:42:29 -0700

    Update to messages.

--- UI-user-feed/src/components/ChatApp.js
+++ UI-user-feed/src/components/ChatApp.js
@@ -33,10 +33,10 @@ class ChatApp extends Component {
 
     componentDidMount() {
         if(localStorage && localStorage.getItem('messages')) {
-            console.log(`React ${ '\u221A' }  ${ '\u2192' } componentDidMount ${ '\u221A' }  ${ '\u2192' }`);
+            console.log(`React componentDidMount ${ '\u221A' }  ${ '\u2192' }`);
             // this.props.getLocalStorage(getMessages); // Call Local Storage after first Get request
         }else {
-            console.log(`React ${ '\u221A' } ${ '\u2192' } componentDidMount ${ '\u221A' }`);
+            console.log(`React componentDidMount ${ '\u221A' }`);
             this.props.getMessages(messagesEndPoint); // Make GET request once ChatApp is rendered
         }
     }
